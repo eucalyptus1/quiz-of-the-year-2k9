9f799e6a4fc1ae64a5f985e9209b7734ba507088
James Harrington <jamesharrington585@hotmail.com>
2022-11-14 18:32:07 -0500

    question appears on button click

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // welcome screen, hidden when quiz starts
-var questionObj = [
+var questionObj = 
     {question: "What's 2 plus 2?",
     answers: ["4", "the force between any two charges is equal to the absolute value of the multiple of the charges divided by four pi times the vacuum permittivity times the distance squared between the two charges", "22", "ice cold refreshments"],
     correct: "4"
-    }
-];
+    };
 
 
-// var quizBody = document.getElementById("quiz-body");
+
+var quizBody = document.getElementById("quiz-body");
 var startBtn = document.getElementById("start-button");
 startBtn.addEventListener("click", startQuiz);
 
@@ -15,18 +15,19 @@ startBtn.addEventListener("click", startQuiz);
 //quiz questions cycle to next when answered
 
 function startQuiz() {
+    
+    startBtn.style.display = "none";
 
-    console.log("click");
-//     startBtn.style.display = none;
+    var questionEl = questionObj.question;
 
-//    var questionThing = document.createElement("p");
-//     questionThing.innerText = questionObj.question;
-//     quizBody.appendChild(questionThing);
+   var questionThing = document.createElement("p");
+    questionThing.textContent = questionEl;
+    quizBody.appendChild(questionThing);
 
 
-//     var answerThing = document.createElement("button");
-//     answerThing.innerText = questionObj.answers[0];
-//     quizBody.appendChild(answerThing);
+    // var answerThing = document.createElement("button");
+    // answerThing.innerText = questionObj.answers.[0];
+    // quizBody.appendChild(answerThing);
 
 }
 
