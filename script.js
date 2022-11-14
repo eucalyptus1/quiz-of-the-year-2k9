24858de08381561008c4d18eda2f7471300d1f1c
James Harrington <jamesharrington585@hotmail.com>
2022-11-14 18:06:18 -0500

    began work on function to start quiz. having trouble with the event listener

---
+++ script.js
@@ -0,0 +1,39 @@
+// welcome screen, hidden when quiz starts
+var questionObj = [
+    {question: "What's 2 plus 2?",
+    answers: ["4", "the force between any two charges is equal to the absolute value of the multiple of the charges divided by four pi times the vacuum permittivity times the distance squared between the two charges", "22", "ice cold refreshments"],
+    correct: "4"
+    }
+];
+
+
+// var quizBody = document.getElementById("quiz-body");
+var startBtn = document.getElementById("start-button");
+startBtn.addEventListener("click", startQuiz);
+
+
+//quiz questions cycle to next when answered
+
+function startQuiz() {
+
+    console.log("click");
+//     startBtn.style.display = none;
+
+//    var questionThing = document.createElement("p");
+//     questionThing.innerText = questionObj.question;
+//     quizBody.appendChild(questionThing);
+
+
+//     var answerThing = document.createElement("button");
+//     answerThing.innerText = questionObj.answers[0];
+//     quizBody.appendChild(answerThing);
+
+}
+
+
+
+//game over screen
+
+//high score initial entry form
+
+//high score page
